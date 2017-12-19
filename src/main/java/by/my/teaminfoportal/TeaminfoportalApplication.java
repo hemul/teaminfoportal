b92603522a870f8a86a4095ce7226613903954ca
Anton Lizunou <antonlizunou@gmail.com>
2017-12-19 23:15:38 +0300

    logging support has been added. initial version of login form submition has been implemented.

--- src/main/java/by/my/teaminfoportal/TeaminfoportalApplication.java
+++ src/main/java/by/my/teaminfoportal/TeaminfoportalApplication.java
@@ -1,12 +1,18 @@
 package by.my.teaminfoportal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TeaminfoportalApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(TeaminfoportalApplication.class);
 
 	public static void main(String[] args) {
+
+		LOG.debug("APP START");
 		SpringApplication.run(TeaminfoportalApplication.class, args);
+		LOG.debug("APP SEND");
 	}
 }
--- src/main/java/by/my/teaminfoportal/controllers/ApiController.java
+++ src/main/java/by/my/teaminfoportal/controllers/ApiController.java
@@ -2,13 +2,17 @@ package by.my.teaminfoportal.controllers;
 
 import by.my.teaminfoportal.model.Visit;
 import by.my.teaminfoportal.repositories.VisitsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+    private static final Logger LOG = LoggerFactory.getLogger(ApiController.class);
     final private VisitsRepository visitsRepository;
 
     public ApiController(VisitsRepository visitsRepository) {
@@ -18,5 +22,12 @@ public class ApiController {
     @PostMapping("/visits")
     public Iterable<Visit> getVisits() {
         return visitsRepository.findAll();
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam(value="username") String username,@RequestParam(value="password") String password) {
+        LOG.debug("Name[{}] and Password[{}]", username, password);
+
+        return username + " " + password;
     }
 }
